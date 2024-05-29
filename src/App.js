87ada57de4f2985bcbb49e854b798e3968fa7e2f
anter-kaur <anterpreet905@gmail.com>
2024-05-29 15:54:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { contractAbi, contractAddress } from './contract';
 
 function App() {
+  const [account, setAccount] = useState('');
+  const [value, setValue] = useState('');
+  const [addValue, setAddValue] = useState(0);
+  // const [contract,setContract]=useState('');
+
+  useEffect(() => {
+    const connectMetamask = async () => {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        const web3 = new Web3(provider);
+        try {
+          await provider.request({ method: "eth_requestAccounts" });
+          const accounts = await web3.eth.getAccounts();
+          setAccount(accounts[0]);
+
+          const contract= new web3.eth.Contract(contractAbi, contractAddress);
+          console.log(account);
+        } catch (error) {
+          console.error("Error while connecting to contract:", error);
+        }
+      } else {
+        console.error("Install Metamask");
+      }
+    };
+
+    connectMetamask();
+  }, []);
+
+// const handleSubmit=async ()=>{
+//   const setval=await contract.methods.set(addValue).send({from:account});
+//           const result = await contract.methods.get().call();
+//           console.log('in effect hook: ',result)
+//           setValue(result.toString());
+// }
+
+
+  const handleSubmit = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      const web3 = new Web3(provider);
+      try {
+        const contract = new web3.eth.Contract(contractAbi, contractAddress);
+        await contract.methods.set(addValue).send({ from: account });
+
+        const result = await contract.methods.get().call();
+        setValue(result.toString());
+        console.log('in submit: ',result)
+      } 
+      catch (error) {
+        console.error("Error while sending transaction:", error);
+      }
+    } 
+    // else {
+    //   console.error("Install Metamask");
+    // }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Smart Contract Integration</h1>
+      <p>Account: {account}</p>
+      <input
+        type='text'
+        onChange={(e) => setAddValue(e.target.value)}
+      />
+      <button type='submit' onClick={handleSubmit}>Submit</button>
+      <p>Value from contract: {value}</p>
     </div>
   );
 }
---
+++ src/contract.js
@@ -0,0 +1,30 @@
+export const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export const contractAddress = '0x02a229870D4F2a22eFA6985B159898d3b115bFA3';
